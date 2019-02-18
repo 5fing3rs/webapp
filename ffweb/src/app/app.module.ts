85bb73c2ba403ddb42eab290a8be6576f4702247
Chitralekha Upadhyay <557961@cognizant.com>
2019-02-18 07:31:55 +0530

    18-Feb-19

--- ffweb/src/app/app.module.ts
+++ ffweb/src/app/app.module.ts
@@ -9,12 +9,14 @@ import {MaterialModule} from './material/material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
